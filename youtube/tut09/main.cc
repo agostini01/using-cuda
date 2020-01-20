66ec16b3b5621bae24a7b47da951baa235b5feaa
Nicolas Agostini <n.b.agostini@gmail.com>
2020-01-20 12:04:43 -0500

    Initialize tut9 folder

---
+++ youtube/tut09/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <stdlib.h>
+#include <chrono>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
+
